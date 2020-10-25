2b4b0227b797dd3ad929a63f0049c85957d9116a
PavelVasilev <vpnprint@gmail.com>
2020-10-26 00:00:03 +0300

    feat: added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  matrix.sort((a, b) => a - b);
+module.exports = function towelSort(matrix) {
+  if (!!matrix && matrix != '') {
+    let newArr = [];
+    let newKArr = [];
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 != 0) {
+        matrix[i].reverse();
+        newKArr.push(matrix[i]);
+      }
+      for (let k = 0; k < matrix[i].length; k++) {
+        newArr.push(matrix[i][k]);
+      }
+    }
+    return newArr;
+  } else {
+    return [];
+  }
 }
